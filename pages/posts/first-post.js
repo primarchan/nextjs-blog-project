80a80824ec71b4ce282c5a5f72ab279b52a32ae2
An T.G <boltcarrier@naver.com>
2023-06-26 07:35:06 +0900

    Layout - Styling

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Layout from '../../components/Layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>첫 번째 글</title>
+      </Head>
+      <h1>첫 번째 글</h1>
+    </Layout>
+  )
+}
